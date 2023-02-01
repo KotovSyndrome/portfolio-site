c46dddbad49df02d1a41619bbc7541cb4cecd100
Payam Katoozian <einpanzerattacken@gmail.com>
2023-02-01 11:11:06 -0800

    phase 1 complete, auto-discovery of new files and refactored page object

--- build.py
+++ build.py
@@ -1,48 +1,39 @@
-#metadata
-pages = [
-    {
-        "filename": "content/contact.html",
-        "output": "docs/contact.html",
-        "title": "Contact Form",
-        "navbar1": "Bio",
-        "navbar2": "Projects",
-        "navbar3": "- Contact -"
-    },
-    {
-        "filename": "content/index.html",
-        "output": "docs/index.html",
-        "title": "Homepage",
-        "navbar1": "- Bio -",
-        "navbar2": "Projects",
-        "navbar3": "Contact"
-    },
-    {
-        "filename": "content/projects.html",
-        "output": "docs/projects.html",
-        "title": "My Projects",
-        "navbar1": "Bio",
-        "navbar2": "- Projects -",
-        "navbar3": "Contact"
-    }
-]
+import glob
+import os
 
 def main():
-    template = open_read_stream("templates/base.html")
+    all_html_files = glob.glob('content/*.html')
+    pages = init_page_object(all_html_files)
+    template = open("templates/base.html").read()
 
     for page in pages:
-        page_contents = open_read_stream(page["filename"])
-        new_contents = create_page(page_contents, template, page["title"], page["navbar1"], page["navbar2"], page["navbar3"])
-        open_write_stream(page["output"], new_contents)
+        page_contents = open(page["filename"]).read()
+        new_contents = create_page(page_contents, template, page)        
+        open(page["output"], "w+").write(new_contents)
 
-def create_page(page, template, title, navbar1, navbar2, navbar3):
-    new_html = template.replace("{{content}}", page).replace("{{title}}", title).replace("{{navbar1}}", navbar1).replace("{{navbar2}}", navbar2).replace("{{navbar3}}", navbar3)
+def init_page_object(html_files):
+    pages = []
+    
+    for file in html_files: 
+        file_name = os.path.basename(file) # ex. index.html
+        title = str(os.path.splitext(file_name)).capitalize() # ex. Index
+        output = "docs/" + file_name # ex. "docs/index.html"
+        pages.append({
+            "filename": file,
+            "title": title,
+            "output": output,
+            "navbar": {
+                "index": "- Bio -" if file_name == "index.html" else "Bio",
+                "projects": "- Projects -" if file_name == "projects.html" else "Projects",
+                "contact": "- Contact -" if file_name == "contact.html" else "Contact",
+            }
+        })
+
+    return pages
+
+def create_page(page_contents, template, page):
+    new_html = template.replace("{{content}}", page_contents).replace("{{title}}", page["title"]).replace("{{navbar1}}", page["navbar"]["index"]).replace("{{navbar2}}", page["navbar"]["projects"]).replace("{{navbar3}}", page["navbar"]["contact"])
     return new_html
-
-def open_read_stream(stream):
-    return open(stream).read()
-
-def open_write_stream(stream, data):
-    return open(stream, "w+").write(data)
 
 if __name__ == "__main__":
     main()

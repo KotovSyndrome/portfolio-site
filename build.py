7ce4b397162d2c3e35744bfda9539af54bcf11db
Payam Katoozian <einpanzerattacken@gmail.com>
2023-02-02 16:19:07 -0800

    phase 3 complete, active link highlighting in effect

--- build.py
+++ build.py
@@ -2,48 +2,40 @@ import glob
 import os
 from jinja2 import Template
 
-def init_page_object(html_files):
+def init_pages_dict(html_files):
     pages = []
     
     for file in html_files: 
         file_name = os.path.basename(file) # ex. index.html
-        title, extension = os.path.splitext(file_name) # ex. Index
-        title = str(title).capitalize()
+        title, extension = os.path.splitext(file_name) 
+        title = str(title).capitalize() # ex. Index
         output = "docs/" + file_name # ex. "docs/index.html"
         pages.append({
-            "filename": file,
+            "filepath": file, # "content/index.html"
+            "filename": file_name,
             "title": title,
             "output": output,
-            "navbar": {
-                "index": "- Bio -" if file_name == "index.html" else "Bio",
-                "projects": "- Projects -" if file_name == "projects.html" else "Projects",
-                "contact": "- Contact -" if file_name == "contact.html" else "Contact",
-            }
         })
 
     return pages
 
-def create_page(page_contents, template, page):
-    # new_html = template.replace("{{content}}", page_contents).replace("{{title}}", page["title"]).replace("{{navbar1}}", page["navbar"]["index"]).replace("{{navbar2}}", page["navbar"]["projects"]).replace("{{navbar3}}", page["navbar"]["contact"])
+def create_page(page_contents, template, all_pages, current_page):
     new_html = template.render(
-        content=page_contents,
-        title=page["title"],
-        navbar1=page["navbar"]["index"],
-        navbar2=page["navbar"]["projects"],
-        navbar3=page["navbar"]["contact"],
+        content      = page_contents,
+        pages        = all_pages,
+        current_page = current_page,
     )
     return new_html
 
 def main():    
     all_html_files = glob.glob('content/*.html')
-    pages = init_page_object(all_html_files)
+    pages = init_pages_dict(all_html_files)
     template_html = open("templates/base.html").read()
     template = Template(template_html)
 
     for page in pages:
-        print(page)
-        page_contents = open(page["filename"]).read()
-        new_contents = create_page(page_contents, template, page)        
+        page_contents = open(page["filepath"]).read()
+        new_contents = create_page(page_contents, template, pages, page["title"])        
         open(page["output"], "w+").write(new_contents)
 
 
